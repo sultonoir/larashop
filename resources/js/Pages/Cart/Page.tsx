03586cf59811f07b55e39508ae54de54863a8878
Sulton <sultonalidrus5@gmail.com>
2025-01-07 18:42:42 +0700

    feat : add empty cart

--- resources/js/Pages/Cart/Page.tsx
+++ resources/js/Pages/Cart/Page.tsx
@@ -14,13 +14,27 @@ const Page = () => {
     <Authenticated>
       <Head title="Cart" />
       <div className="relative mx-auto min-h-screen max-w-screen-lg">
-        <div className="flex flex-col justify-between gap-4 md:flex-row lg:gap-10">
-          <div className="order-2 flex w-full flex-col gap-2 md:order-1">
-            {cart.map((product, index) => (
-              <CartCard product={product} key={index} />
-            ))}
-          </div>
+        <div className="flex h-96 w-full flex-col items-center justify-center gap-5">
+          {cart.length < 1 ? (
+            <>
+              <p className="text-2xl lg:text-4xl">
+                your shopping cart is empty
+              </p>
+              <Link
+                href="/product"
+                className="bg-primary rounded-lg px-4 py-2 text-white">
+                Explore now
+              </Link>
+            </>
+          ) : (
+            <div className="flex w-full flex-col gap-2 md:order-1">
+              {cart.map((product, index) => (
+                <CartCard product={product} key={index} />
+              ))}
+            </div>
+          )}
         </div>
+
         <div className="mt-5 p-5">
           <div className="flex flex-col items-center justify-between gap-4 rounded-lg bg-zinc-50 p-4 md:flex-row">
             <p className="text-lg lg:text-2xl">
